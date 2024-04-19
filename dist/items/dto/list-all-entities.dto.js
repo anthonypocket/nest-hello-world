15c277752c60f3b2d1b189e06ecbf0560db22a5b
Tony <diltony@yahoo.com>
2024-04-19 19:10:35 +0100

    update

--- dist/items/dto/list-all-entities.dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ListAllEntities {
-}
-exports.ListAllEntities = ListAllEntities;
-//# sourceMappingURL=list-all-entities.dto.js.map
